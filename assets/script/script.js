ff59b9a2b18de6f0b20f1431995926ce80095dbb
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-08-05 18:11:13 -0400

    created function getUserOptions()

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,70 +1,123 @@
 // Pseudocode
 // Array of special characters
 var specialChar = [
-  "!",
-  "@",
+  '!',
+  '@',
   '"',
-  "#",
-  "$",
-  "%",
-  "&",
+  '#',
+  '$',
+  '%',
+  '&',
   "'",
-  "(",
-  ")",
-  "*",
-  "+",
-  ",",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "<",
-  ">",
-  "?",
-  "[",
-  "\\",
-  "]",
-  "^",
-  "_",
-  "`",
-  "{",
-  "|",
-  "}",
-  "~",
-];
+  '(',
+  ')',
+  '*',
+  '+',
+  ',',
+  '-',
+  '.',
+  '/',
+  ':',
+  ';',
+  '<',
+  '<',
+  '>',
+  '?',
+  '[',
+  '\\',
+  ']',
+  '^',
+  '_',
+  '`',
+  '{',
+  '|',
+  '}',
+  '~',
+]
 
 //Array of numerical characters
-var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 // Array of lower characters (look up .split())
-var lowerChar = "abcdefghijklmnopqrstuvwxyz".split("");
+var lowerChar = 'abcdefghijklmnopqrstuvwxyz'.split('')
 // Array of upperCase characters (look up .split())
-var upperChar = "ABCDEFGHIJKLMNOPQRSTUVQXYZ".split("");
+var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVQXYZ'.split('')
 
 // Create a function that prompts the user for the password options
 function getUserOptions() {
   //Create a var to store length of password from the user input (look up parseInt())
-  // var passLength =
+  var passLength = parseInt(prompt('How long do you need your password?'))
   // create a conditional statement to check lenght is actual number  ie 8 eight
   // create a conditional to check if password length is atleast 8 chars
   // create a conditional to check if pw length is lower than 128 chars
+  if (passLength > 0 && passLength < 129) {
+    console.log(passLength)
+  } else {
+    alert('Please enter valid number between 8 and 128 characters!')
+    getUserOptions()
+  }
+
   // create 4 different conditional statements to store if user pass is going to use special chars, nums, lower, and upper
+
+  // Special Chars
+  var isSpecial = prompt('Does your password need special characters?')
+  if (isSpecial.toLowerCase() == 'yes' || isSpecial.toLowerCase() == 'no') {
+    console.log('isSpecial: ' + isSpecial)
+  } else {
+    alert('Please pick yes or no!')
+    console.log('isSpecial: ' + isSpecial)
+    getUserOptions()
+  }
+
+  // Num
+  var isNum = prompt('Does your password need numerical characters?')
+  if (isNum.toLowerCase() === 'yes' || isNum.toLowerCase() === 'no') {
+    console.log('isNum: ' + isNum)
+  } else {
+    alert('Please pick yes or no!')
+    getUserOptions()
+  }
+  // Lower
+  var isLower = prompt('Does your password need lowercase characters?')
+  if (isLower.toLowerCase() === 'yes' || isLower.toLowerCase() === 'no') {
+    console.log('isLower: ' + isLower)
+  } else {
+    alert('Please pick yes or no!')
+    getUserOptions()
+  }
+  //Upper
+  var isUpper = prompt('Does your password need uppercase characters?')
+  if (isUpper.toLowerCase() === 'yes' || isUpper.toLowerCase() === 'no') {
+    console.log('isUpper: ' + isUpper)
+  } else {
+    alert('Please pick yes or no!')
+    getUserOptions()
+  }
   // create a conditional statement to check if user included some type of chars (if else?)
+  if (
+    isSpecial === 'no' &&
+    isNum === 'no' &&
+    isLower === 'no' &&
+    isUpper === 'no'
+  ) {
+    alert('Your password can not contain no characters!')
+    getUserOptions()
+  }
   // Create a var to store the user input
   // Create object to store user input
-  // var passOptions = {
-  //   passLength: passLenght,
-  //   specialChars,
-  //   numericalChars,
-  //   lowerCase,
-  //   upperCase,
-  // };
-  // return our passOptions
+  var passOptions = {
+    passLength: passLength,
+    isSpecial: isSpecial,
+    isNum: isNum,
+    isLower: isLower,
+    isUpper: isUpper,
+  }
+  console.log(passOptions)
+  return passOptions
 }
 
 // function for getting a random element from an array
 // check out math.random
+function randomizer() {}
 
 // function to generate a password with our user input
 //function generatePassword{
@@ -101,15 +154,15 @@ function getUserOptions() {
 // Take the result Look up join() and turn it into a string
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate')
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword()
+  var passwordText = document.querySelector('#password')
 
-  passwordText.value = password;
+  passwordText.value = password
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword)
