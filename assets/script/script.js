10fc88d5c2ebf1233acd1dfef0efcd137899ce09
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-08-06 20:11:38 -0400

    created pass generator function and added printed to html

--- assets/script/script.js
+++ assets/script/script.js
@@ -35,18 +35,18 @@ var specialChar = [
   "~",
 ];
 
-
 var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; //Array of numerical characters
 var lowerChar = "abcdefghijklmnopqrstuvwxyz".split(""); // Array of lower characters (look up .split())
 var upperChar = "ABCDEFGHIJKLMNOPQRSTUVQXYZ".split(""); // Array of upperCase characters (look up .split())
+var passwordValue;
 
 // Create a function that prompts the user for the password options
-function getUserOptions() { 
+function getUserOptions() {
   var passLength = parseInt(prompt("How long do you need your password?")); //Create a var to store length of password from the user input (look up parseInt())
   // create a conditional statement to check lenght is actual number  ie 8 eight
   // create a conditional to check if password length is atleast 8 chars
   // create a conditional to check if pw length is lower than 128 chars
-  if (passLength > 0 && passLength < 129) {
+  if (passLength > 8 && passLength < 129) {
     console.log(passLength);
   } else {
     alert("Please enter valid number between 8 and 128 characters!");
@@ -58,11 +58,11 @@ function getUserOptions() {
   // Special Chars
   var isSpecial = prompt("Does your password need special characters?");
   // if (isSpecial.toLowerCase() == "yes" || isSpecial.toLowerCase() == "no") {
-  if (isSpecial.toLowerCase() == "yes" ){
-    isSpecial=true;
+  if (isSpecial.toLowerCase() == "yes") {
+    isSpecial = true;
     console.log("isSpecial: " + isSpecial);
-  } else if(isSpecial.toLowerCase() == "no") {
-    isSpecial=false;
+  } else if (isSpecial.toLowerCase() == "no") {
+    isSpecial = false;
     console.log("isSpecial: " + isSpecial);
   } else {
     alert("Please pick yes or no!");
@@ -73,7 +73,7 @@ function getUserOptions() {
   // Num
   var isNum = prompt("Does your password need numerical characters?");
   // if (isNum.toLowerCase() === "yes" || isNum.toLowerCase() === "no") {
-  if (isNum.toLowerCase() === "yes" ){
+  if (isNum.toLowerCase() === "yes") {
     isNum = true;
     console.log("isNum: " + isNum);
   } else if (isNum.toLowerCase() === "no") {
@@ -100,7 +100,7 @@ function getUserOptions() {
   var isUpper = prompt("Does your password need uppercase characters?");
   // if (isUpper.toLowerCase() === 'yes' || isUpper.toLowerCase() === 'no') {
   if (isUpper.toLowerCase() === "yes") {
-    isSpecial = true;
+    isUpper = true;
     console.log("isUpper: " + isUpper);
   } else if (isUpper.toLowerCase() === "no") {
     isUpper = false;
@@ -121,7 +121,8 @@ function getUserOptions() {
   }
 
   // Create a var to store the user input
-  var passOptions = {   // Create object to store user input
+  var passOptions = {
+    // Create object to store user input
     passLength: passLength,
     isSpecial: isSpecial,
     isNum: isNum,
@@ -132,8 +133,7 @@ function getUserOptions() {
   return passOptions;
 }
 
-// function for getting a random element from an array
-// check out math.random
+// function for getting a random element from an array (check out math.random)
 function randomizer(char) {
   var randEl = char[Math.floor(Math.random() * char.length)];
   console.log("random element from array " + randEl);
@@ -145,48 +145,80 @@ function randomizer(char) {
 
 // function to generate a password with our user input
 function generatePassword() {
-  
   var userOptions = getUserOptions(); // create a variable and call our function so we can use our data from the previous function.
   var results = []; // create a var to store password
-  var userPossibleChars = []; // array to store the types of characters to include in our password 
-  var guarChar = []; // array to contain atleast one of each chosen type of characters to make sure atleast one of every char is being used (validation)
+  var userPossibleChars = []; // array to store the types of characters to include in our password
+  // var guarChar = []; // array to contain atleast one of each chosen type of characters to make sure atleast one of every char is being used (validation)
 
   // create conditional statements that add the array of characters into an array of possible characters based on our users input
   // need to push new random characters to the guaranteed characters (look up .concat())
-   if(userOptions.isSpecial){  
-     userPossibleChars.concat(specialChar); //take chars and concat
-     console.log(userPossibleChars);
-  
-  // take chars and push(randomizationfunction(specialChars) (after we randomize)
-    
+  if (userOptions.isSpecial) {
+    userPossibleChars = userPossibleChars.concat(specialChar); //take chars and concat
+    console.log("specialChar added to userPossibleChars " + userPossibleChars);
+
+    // take chars and push(randomizationfunction(specialChars) (after we randomize)
+  } else {
+    console.log("No special characters required");
   }
 
   // create conditional statements that add the array of characters into an array of possible characters based on our users input
   // need to push new random characters to the guaranteed characters (look up .concat())
-  if(userOptions.isNum){
-    userPossibleChars.concat(numChar);
-    console.log(userPossibleChars);
+  if (userOptions.isNum) {
+    userPossibleChars = userPossibleChars.concat(numChar);
+    console.log("numChar added to userPossibleChars " + userPossibleChars);
+  } else {
+    console.log("No number characters required");
   }
 
   // create conditional statements that add the array of characters into an array of possible characters based on our users input
   // need to push new random characters to the guaranteed characters (look up .concat())
-  if(userOptions.isLower){
-    userPossibleChars.concat(lowerChar);
-    console.log(userPossibleChars);
+  if (userOptions.isLower) {
+    userPossibleChars = userPossibleChars.concat(lowerChar);
+    console.log("lowerChar added to userPossibleChars " + userPossibleChars);
+  } else {
+    console.log("No lowercase characters required");
   }
   // create conditional statements that add the array of characters into an array of possible characters based on our users input
   // need to push new random characters to the guaranteed characters (look up .concat())
-  if(userOptions.isUpper){
-    userPossibleChars.concat(upperChar);
-    console.log(userPossibleChars);
+  if (userOptions.isUpper) {
+    userPossibleChars = userPossibleChars.concat(upperChar);
+    console.log("upperChar added to userPossibleChars " + userPossibleChars);
+  } else {
+    console.log("No uppercase characters required");
   }
-
-  // create a for loop to pluck out random options object and grabing random characters from the array of possible characters and concat them into the results variable
 
   // create another for loop to guarantee atleast one character from each possible characters in the result
+  if(userOptions.isSpecial){
+    results.push(randomizer(specialChar));
+  } else if(userOptions.isNum){
+    results.push(randomizer(numChar));
+  } else if(userOptions.isLower){
+    results.push(randomizer(lowerChar));
+  }else if(userOptions.isUpper){
+    results.push(randomizer(upperChar));
+  }
 
-  // Take the result Look up join() and turn it into a string
+  if(results.length == userOptions.passLength){
+     this.passwordValue = results.join('');
+  }  else{
+    var iterateLength = (parseInt(userOptions.passLength) - results.length); 
+    // create a for loop to pluck out random options object and grabing random characters from the array of possible characters and concat them into the results variable
+    for(var i = 0 ; i < iterateLength; i++){ 
+      results.push(randomizer(userPossibleChars));
+    }
+
+    if(results.length == userOptions.passLength){
+      console.log(results.join(''));
+      this.passwordValue = results.join('');
+    } else{
+      return 'You made a mistake!';
+    }
+  } 
+
+document.getElementById("password").value = this.passwordValue;  
 }
+
+
 
 // Assignment Code
 //var generateBtn = document.querySelector('#generate')
